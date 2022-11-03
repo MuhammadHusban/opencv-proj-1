efdc8f828f4dbd7e3409cc17187e9515efaa3b4c
MuhammadHusban <husban2006@gmail.com>
2022-11-04 00:15:04 +0500

    opencv

--- rescale.py
+++ rescale.py
@@ -1,26 +1,29 @@
-from turtle import width
-import cv2 as cv
+# from turtle import width
+# import cv2 as cv
 
-# img = cv.imread("images/dog.24.jpg")
-# cv.imshow("dog",img)
-def rescaleImages(frame,scale = 1):
-    width =frame.shape[1] * scale
-    height = frame.shape[0] * scale
 
-    dimensions = (width,height)
+# def rescaleImages(frame,scale = 1):
+#     width =int(frame.shape[1] * scale)
+#     height = int(frame.shape[0] * scale)
+
+#     dimensions = (width,height)
     
-    return cv.resize(frame,dimensions,interpolation=cv.INTER_AREA)
-# cv.waitKey(0)
-capture = cv.VideoCapture("videos\pexels-marc-espejo-6548176.mp4")
-while True:
-    isTrue,frame = capture.read()
-    frame_resized = rescaleImages(frame)
+#     return cv.resize(frame,dimensions,interpolation=cv.INTER_AREA)
+# def changeRes(width,height):                           
+#     capture.set(3,width)
+#     capture.set(4,height)
 
 
-    cv.imshow("Video",frame)
-    cv.imshow("Video Resized",frame_resized)
+# capture = cv.VideoCapture("videos\pexels-marc-espejo-6548176.mp4")
+# while True:
+#     isTrue,frame = capture.read()
+#     frame_resized = rescaleImages(frame)
 
-    if cv.waitKey(20) & 0xFF==ord("d"):
-        break
-capture.release()
-cv.destroyAllWindows()
+
+#     cv.imshow("Video",frame)
+#     cv.imshow("Video Resized",frame_resized)
+
+#     if cv.waitKey(20) & 0xFF==ord("d"):
+#         break
+# capture.release()
+# cv.destroyAllWindows()
